1df7c2850137a811877d73db34d5558df8191426
jdeolive <jdeolive@gmail.com>
2016-07-07 20:19:15 -0600

    Updated behaviour around auto provision flag.
    When not set we require the user to exist.

--- src/community/xauth/src/test/java/org/geoserver/security/xauth/XAuthFilterTest.java
+++ src/community/xauth/src/test/java/org/geoserver/security/xauth/XAuthFilterTest.java
@@ -1,8 +1,14 @@
 package org.geoserver.security.xauth;
 import org.geoserver.security.GeoServerRoleService;
 import org.geoserver.security.GeoServerSecurityManager;
+import org.geoserver.security.GeoServerUserGroupService;
+import org.geoserver.security.GeoServerUserGroupStore;
 import org.geoserver.security.auth.AuthenticationCache;
+import org.geoserver.security.config.PasswordPolicyConfig;
 import org.geoserver.security.impl.GeoServerRole;
+import org.geoserver.security.impl.GeoServerUser;
+import org.geoserver.security.validation.PasswordValidatorImpl;
+import org.junit.Before;
 import org.junit.Test;
 import org.springframework.mock.web.MockFilterChain;
 import org.springframework.mock.web.MockHttpServletRequest;
@@ -13,14 +19,23 @@ import org.springframework.security.core.context.SecurityContextHolder;
 
 import java.util.Collections;
 
+import static org.easymock.EasyMock.anyObject;
+import static org.easymock.EasyMock.eq;
+import static org.easymock.EasyMock.expectLastCall;
 import static org.easymock.classextension.EasyMock.createNiceMock;
 import static org.easymock.classextension.EasyMock.replay;
 import static org.easymock.classextension.EasyMock.expect;
+import static org.easymock.classextension.EasyMock.verify;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 
 public class XAuthFilterTest {
+
+  @Before
+  public void setup() {
+    SecurityContextHolder.getContext().setAuthentication(null);
+  }
 
   @Test
   public void sanity() throws Exception {
@@ -32,11 +47,19 @@ public class XAuthFilterTest {
     GeoServerRoleService roleService = createNiceMock(GeoServerRoleService.class);
     expect(roleService.getRolesForUser("bob")).andReturn(Collections.emptySortedSet()).anyTimes();
 
-    expect(secMgr.getActiveRoleService()).andReturn(roleService).anyTimes();
+    GeoServerUser bob = createNiceMock(GeoServerUser.class);
 
-    replay(authCache, roleService, secMgr);
+    GeoServerUserGroupService ugService = createNiceMock(GeoServerUserGroupService.class);
+    expect(ugService.getUserByUsername("bob")).andReturn(bob).anyTimes();
+
+    expect(secMgr.loadUserGroupService("default")).andReturn(ugService).anyTimes();
+    expect(secMgr.loadRoleService("default")).andReturn(roleService).anyTimes();
+
+    replay(authCache, ugService, roleService, secMgr);
 
     XAuthFilterConfig config = new XAuthFilterConfig();
+    config.setUserGroupServiceName("default");
+    config.setRoleServiceName("default");
     config.setAutoProvisionUsers(false);
 
     XAuthFilter filter = new XAuthFilter();
@@ -61,5 +84,57 @@ public class XAuthFilterTest {
         .findAny()
         .isPresent());
     assertTrue(auth.getAuthorities().stream().filter(GeoServerRole.AUTHENTICATED_ROLE::equalsWithoutUserName).findAny().isPresent());
+  }
+
+  @Test
+  public void testProvisionFlag() throws Exception {
+    GeoServerSecurityManager secMgr = createNiceMock(GeoServerSecurityManager.class);
+
+    PasswordValidatorImpl validator = new PasswordValidatorImpl(secMgr);
+    validator.setConfig(new PasswordPolicyConfig());
+    expect(secMgr.loadPasswordValidator(anyObject(String.class))).andReturn(validator).anyTimes();
+
+    AuthenticationCache authCache = createNiceMock(AuthenticationCache.class);
+    expect(secMgr.getAuthenticationCache()).andReturn(authCache).anyTimes();
+
+    GeoServerRoleService roleService = createNiceMock(GeoServerRoleService.class);
+    expect(roleService.getRolesForUser(anyObject(String.class))).andReturn(Collections.emptySortedSet()).anyTimes();
+
+    GeoServerUser bob = new GeoServerUser("bob");
+
+    GeoServerUserGroupService ugService = createNiceMock(GeoServerUserGroupService.class);
+
+    GeoServerUserGroupStore ugStore = createNiceMock(GeoServerUserGroupStore.class);
+    expect(ugStore.createUserObject(eq("bob"), anyObject(String.class), eq(true))).andReturn(bob).anyTimes();
+    ugStore.addUser(eq(bob));
+    expectLastCall().once();
+
+    expect(ugService.canCreateStore()).andReturn(true).anyTimes();
+    expect(ugService.createStore()).andReturn(ugStore).anyTimes();
+
+    expect(secMgr.loadUserGroupService("default")).andReturn(ugService).anyTimes();
+    expect(secMgr.loadRoleService("default")).andReturn(roleService).anyTimes();
+
+    replay(authCache, ugService, ugStore, roleService, secMgr);
+
+    XAuthFilterConfig config = new XAuthFilterConfig();
+    config.setUserGroupServiceName("default");
+    config.setRoleServiceName("default");
+    config.setAutoProvisionUsers(true);
+
+    XAuthFilter filter = new XAuthFilter();
+    filter.setSecurityManager(secMgr);
+    filter.initializeFromConfig(config);
+
+    MockHttpServletRequest req = new MockHttpServletRequest();
+    req.addHeader("x-auth-user", "bob");
+    req.addHeader("x-auth-roles", "ADMIN");
+
+    filter.doFilter(req, new MockHttpServletResponse(), new MockFilterChain());
+
+    Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+    assertNotNull(auth);
+
+    verify(ugStore);
   }
 }
